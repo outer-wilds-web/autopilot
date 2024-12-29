c7c7930091340c93a30609713b0b23e37dd4cc2d
SOULIER Patrice <p.patrice976@hotmail.com>
2024-12-29 14:25:44 +0100

    Fix bug: loss websocket-connexion after ~20s

--- websocket_client.py
+++ websocket_client.py
@@ -5,7 +5,7 @@ from autopilot.planet_autopilot import PlanetAutopilot
 
 async def websocket_handler(autopilot_class, *args, verbose=0):
     uri = "ws://127.0.0.1:3012"
-    async with websockets.connect(uri) as websocket:
+    async with websockets.connect(uri, ping_timeout=None) as websocket:
         print("Connecté au serveur WebSocket")
         autopilot = autopilot_class(websocket, *args, verbose)
         await autopilot.run()
